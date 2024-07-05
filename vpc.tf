825bfc84a25504aef98350612fd8d4486699cc37
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-05 13:04:34 +0530

    route53

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "../expense-vpc"
+  # source = "../expense-vpc"
+  source = "git::https://github.com/rajeshkalavapalli/expense-vpc.git?ref=main"
   common_tags = var.common_tags
   project_name = var.project_name
   public_subnet_cidrs = var.public_subnet_cidrs
